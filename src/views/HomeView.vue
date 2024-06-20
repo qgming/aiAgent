ffe1b9d0c3ea43e8b15144acb3e4341053cdf4de
qgming <3164520781@qq.com>
2024-06-21 00:07:05 +0800

    ces

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -99,7 +99,8 @@ export default {
   },
   methods: {
     getImageUrl(path) {
-      return new URL(path, import.meta.url).href;
+      return require(path);
+      // return new URL(path, import.meta.url).href;
     },
     getToolCards(categoryIndex) {
       return this.toolCards[categoryIndex - 1] || [];
