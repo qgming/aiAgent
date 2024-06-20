240a691f11aa72a5276b0d738ae31fc512904268
qgming <3164520781@qq.com>
2024-06-21 00:58:25 +0800

    ceshi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,8 @@
           <div class="anyCard">
             <div class="toolCard" @click="openTools(tool)" v-for="tool in getToolCards(index + 1)" :key="tool.id">
               <div class="logoBox">
-                <img class="toolLogo" :src=getImageUrl(tool.logo) alt="">
+                <!-- <img class="toolLogo" :src=getImageUrl(tool.logo) alt=""> -->
+                <img class="toolLogo" :src="tool.logo" alt="logo">
               </div>
               <div class="cardText">
                 <p class="cardName">{{ tool.name }}</p>
@@ -56,10 +57,10 @@ export default {
       toolCards: [
         // 编辑精选
         [
-          { id: 1, logo: "../assets/images/biganzi.png", name: "公文笔杆子", more: '提升效率，驾驭文书之道', prompt: '../prompt/002/gongwen.txt' },
-          { id: 4, logo: "../assets/images/wechat.png", name: "公众号文章", more: '公众号写作助理', prompt: '../prompt/001/gongzhonghao.txt' },
-          { id: 2, logo: "../assets/images/ai.png", name: "翻译器", more: '一键中英互译，直译意译，样样精通', prompt: '../prompt/002/fanyi.txt' },
-          { id: 3, logo: "../assets/images/xiaohongshu.png", name: "小红书生成器", more: '小红书爆款生成器', prompt: '../prompt/001/xiaohongshu.txt' },
+          { id: 1, logo: "../images/biganzi.png", name: "公文笔杆子", more: '提升效率，驾驭文书之道', prompt: '../prompt/002/gongwen.txt' },
+          { id: 4, logo: "../images/wechat.png", name: "公众号文章", more: '公众号写作助理', prompt: '../prompt/001/gongzhonghao.txt' },
+          { id: 2, logo: "../images/ai.png", name: "翻译器", more: '一键中英互译，直译意译，样样精通', prompt: '../prompt/002/fanyi.txt' },
+          { id: 3, logo: "../images/xiaohongshu.png", name: "小红书生成器", more: '小红书爆款生成器', prompt: '../prompt/001/xiaohongshu.txt' },
 
         ],
         // 新媒体
@@ -98,13 +99,13 @@ export default {
     };
   },
   methods: {
-    getImageUrl(path) {
-      console.log(path);
-      return require(path);
-      // console.log(new URL(path, import.meta.url).pathname);
-      // console.log(new URL(path, import.meta.url).href);
-      // return new URL(path, import.meta.url).pathname;
-    },
+    // getImageUrl(path) {
+    //   console.log(path);
+    //   // return require(path);
+    //   // console.log(new URL(path, import.meta.url).pathname);
+    //   // console.log(new URL(path, import.meta.url).href);
+    //   // return new URL(path, import.meta.url).pathname;
+    // },
     getToolCards(categoryIndex) {
       return this.toolCards[categoryIndex - 1] || [];
     },
